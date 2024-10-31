72a9c1bd3675df33391fa99d5888608bf2a213db
adharainfinita <adharanosalevich@gmail.com>
2024-10-31 11:39:30 -0300

    xzcfdfs

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors({
-  origin: 'https://aromas-frescos-rfh06hpiu-adharainfinitas-projects.vercel.app/',
+  origin: 'https://aromas-frescos.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }))
 
