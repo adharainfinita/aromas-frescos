28e3a34fc1a7e7477913407ffc7ba4864bdfe859
adharainfinita <adharanosalevich@gmail.com>
2024-11-03 14:06:43 -0300

    change pg env

--- api/src/db/db.ts
+++ api/src/db/db.ts
@@ -2,11 +2,11 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,            // Cambia esto por tu usuario de PostgreSQL
-  host: process.env.DB_HOST,              // Cambia esto si usas otro host
-  database: process.env.DB_DATABASE,  // Cambia esto por el nombre de tu base de datos
-  password: process.env.DB_PASSWORD,      // Cambia esto por tu contraseña de PostgreSQL
-  port: Number(process.env.DB_PORT),                     // Puerto predeterminado de PostgreSQL
+  user: process.env.PGUSER,            // Cambia esto por tu usuario de PostgreSQL
+  host: process.env.PGHOST,              // Cambia esto si usas otro host
+  database: process.env.PGDATABASE,  // Cambia esto por el nombre de tu base de datos
+  password: process.env.PGPASSWORD,      // Cambia esto por tu contraseña de PostgreSQL
+  port: Number(process.env.PGPORT),                     // Puerto predeterminado de PostgreSQL
 });
 
 // Función para obtener una conexión a la base de datos
