113d1ed504bea7119b475a6b2d4d1c0342ab27ae
adharainfinita <adharanosalevich@gmail.com>
2024-11-07 11:24:03 -0300

    now yes

--- api/src/middlewares/customersValidator.ts
+++ api/src/middlewares/customersValidator.ts
@@ -2,7 +2,6 @@ import { body, param } from 'express-validator';
 
 export const validateCreateCustomer = [
   body('name').notEmpty().withMessage('El nombre es obligatorio'),
-  body('email').isEmail().optional().withMessage('Debe ser un email válido'),
   body('phone').isMobilePhone('any').withMessage('Debe ser un número de teléfono válido')
 ];
 
