ed22285bbb6035a93e45f27407050a0a61345749
adharainfinita <adharanosalevich@gmail.com>
2024-11-15 11:41:52 -0300

    test filtes sytles2

--- front/src/components/Filters.tsx
+++ front/src/components/Filters.tsx
@@ -1,53 +1,78 @@
-import { Box, Checkbox, FormControlLabel, MenuItem, TextField } from "@mui/material";
-import { filterProductsByCategory, filtersProductByAvailable } from "../redux/features/productsSlice";
+import {
+	Box,
+	Checkbox,
+	FormControlLabel,
+	MenuItem,
+	TextField,
+} from "@mui/material";
+import {
+	filterProductsByCategory,
+	filtersProductByAvailable,
+} from "../redux/features/productsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { orderProducts } from "../redux/features/productsSlice";
 
-const Filters:React.FC = () => {
-  const products = useSelector((state: RootState) => state.products.originalCopy);
+const Filters: React.FC = () => {
+	const products = useSelector(
+		(state: RootState) => state.products.originalCopy
+	);
 	const dispatch = useDispatch();
 
-	const handleCategoryChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+	const handleCategoryChange = (
+		event: React.ChangeEvent<{ value: unknown }>
+	) => {
 		dispatch(filterProductsByCategory(event.target.value as string));
 	};
 
-	const handleAvailabilityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+	const handleAvailabilityChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
 		dispatch(filtersProductByAvailable(event.target.checked ? true : null));
 	};
-  const handleProductOrderChange = (
+	const handleProductOrderChange = (
 		event: React.ChangeEvent<{ value: unknown }>
 	) => {
 		dispatch(orderProducts(event.target.value as string));
 	};
-    return(
-      <Box sx={{ display: "flex", overflowWrap:'break-word', gap: 2, mb: 3 }}>
-      <TextField
-        label="Categoría"
-        select
-        fullWidth
-        value={useSelector((state: RootState) => state.products.requireFilters.category) || ""}
-        onChange={handleCategoryChange}
-      >
-        <MenuItem value="All">Todas</MenuItem>
-        {Array.from(new Set(products.map((p) => p.product_category))).map(
-          (category) => (
-            <MenuItem key={category} value={category}>
-              {category}
-            </MenuItem>
-          )
-        )}
-      </TextField>
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={useSelector((state: RootState) => state.products.requireFilters.availbable) ?? false}
-            onChange={handleAvailabilityChange}
-          />
-        }
-        label="Disponibles"
-      />
-      <TextField
+	return (
+		<>
+			<Box sx={{ display: "flex", overflowWrap: "break-word", gap: 2, mb: 3 }}>
+				<TextField
+					label="Categoría"
+					select
+					fullWidth
+					value={
+						useSelector(
+							(state: RootState) => state.products.requireFilters.category
+						) || ""
+					}
+					onChange={handleCategoryChange}
+				>
+					<MenuItem value="All">Todas</MenuItem>
+					{Array.from(new Set(products.map((p) => p.product_category))).map(
+						(category) => (
+							<MenuItem key={category} value={category}>
+								{category}
+							</MenuItem>
+						)
+					)}
+				</TextField>
+				<FormControlLabel
+					control={
+						<Checkbox
+							checked={
+								useSelector(
+									(state: RootState) => state.products.requireFilters.availbable
+								) ?? false
+							}
+							onChange={handleAvailabilityChange}
+						/>
+					}
+					label="Disponibles"
+				/>
+			</Box>
+			<TextField
 				label="Orden"
 				select
 				fullWidth
@@ -57,8 +82,8 @@ const Filters:React.FC = () => {
 				<MenuItem value="A">Ascendente</MenuItem>
 				<MenuItem value="D">Descendente</MenuItem>
 			</TextField>
-    </Box>
-    )
-}
+		</>
+	);
+};
 
 export default Filters;
